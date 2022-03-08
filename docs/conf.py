4b9b7eeaeef56ca2610fd62c90ada9aa4b5f3312
joshuaneronha <77899869+joshuaneronha@users.noreply.github.com>
2022-03-08 13:48:03 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from recommonmark.parser import CommonMarkParser
 
+source_parsers = {'.md': CommonMarkParser}
+
+source_suffix = ['.rst', '.md']
 
 # -- Project information -----------------------------------------------------
 
