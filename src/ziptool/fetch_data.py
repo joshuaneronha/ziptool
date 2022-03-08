d8e8c6345badc9907257af4b98db7b6d7d275092
Kevin Wilson <khwilson@gmail.com>
2022-03-08 10:36:48 -0500

    Some comments
    This is great! I've done some basic cleanup here that consists of the
    following:
    
      * Ran black and isort across the whole repo to clean it up
      * Removed unused dependencies
      * Added a `utils` file (I hate the generic `utils` file but it's
        useful) to pull out common utilities
      * Converted the Excel file (which took forever to load) to a parquet
        file (which takes much less time to load)
      * Clean up some docstrings
      * Kept lines to 88 characters
      * Converted a bunch of string concats to f-strings for readability
      * Added a couple tests
      * Removed some extraneous cruft (e.g., .DS_Store)
      * Added `pyarrow` as a dependency while removing `openpyxl`
    
    One general comment here is that I don't think we've quite figured out
    the caching situation. In particular, `os.chdir` should basically never
    be called from a library. In general, any _global_ setting is very
    dangerous to call from such a location. So we need to figure out a
    slightly better way to handle this.
    
    Also, you're missing a file in your commit (the usa_00013.csv file) to
    actually run your tests, so I couldn't run them all front to back :-)

--- src/ziptool/fetch_data.py
+++ src/ziptool/fetch_data.py
@@ -1,29 +1,20 @@
-import tempfile
-import json
-import shutil
-import urllib
-from functools import lru_cache
 import os
 from os.path import exists
 from pathlib import Path
-from typing import Dict, List, Union
-import pkg_resources
-import tempfile
+from typing import Optional, Union
 
-import geopandas as gpd
-import numpy as np
-import pandas as pd
 import requests
-import us
-import wquantiles
 
-pd.options.mode.chained_assignment = None
+from ziptool.utils import cast_fips_code
 
-from ziptool import shp_dir
+from . import shp_dir
 
 FilenameType = Union[str, Path]
 
-def download_file(url: str, output_filename: FilenameType):
+
+def download_file(
+    url: str, output_filename: FilenameType, session: Optional[requests.Session] = None
+):
     """
     Downloads a file from the provided URL and saves it at the desired path.
 
@@ -35,19 +26,26 @@ def download_file(url: str, output_filename: FilenameType):
         None
     """
 
+    # TODO (khw): Generally a very bad idea to call chdir, especially from a library
     os.chdir(shp_dir.name)
 
-    with requests.get(url, stream=True) as response:
+    session = session or requests
+
+    with session.get(url, stream=True) as response:
         response.raise_for_status()
         with open(output_filename, "wb") as outfile:
             for chunk in response.iter_content(chunk_size=8192):
                 outfile.write(chunk)
 
+
 def get_shape_files(state_fips_code):
     """
+    For a given state (in particular its FIPS code), downloads its census tracts and
+    PUMA shapefiles from the Census Bureau. The functions skips the download if the
+    file already has been fetched!
 
-    For a given state (in particular its FIPS code), downloads its census tracts and PUMA shapefiles
-    from the Census Bureau. The functions skips the download if the file already has been fetched!
+    Note that we are pulling the 2019 shapefiles. This function should be updated for
+    other years' shapefiles
 
     Args:
         state_fips_code: string representing the state of interest
@@ -55,16 +53,16 @@ def get_shape_files(state_fips_code):
     Returns:
         Saves .shp files for both PUMA and census tracts within the data directory.
     """
+    state_fips_code = cast_fips_code(state_fips_code)
 
-
-    tract_file = "tl_2019_" + state_fips_code + "_tract"
-    puma_file = "tl_2019_" + state_fips_code + "_puma10"
+    tract_file = f"tl_2019_{state_fips_code}_tract"
+    puma_file = f"tl_2019_{state_fips_code}_puma10"
 
     if exists(tract_file + ".zip"):
-        pass
-    else:
-        puma_url = "https://www2.census.gov/geo/tiger/TIGER2019/PUMA/" + puma_file + ".zip"
-        download_file(puma_url, puma_url.split('/')[-1])
+        return
 
-        tract_url = "https://www2.census.gov/geo/tiger/TIGER2019/TRACT/" + tract_file + ".zip"
-        download_file(tract_url, tract_url.split('/')[-1])
+    puma_url = f"https://www2.census.gov/geo/tiger/TIGER2019/PUMA/{puma_file}.zip"
+    download_file(puma_url, puma_url.split("/")[-1])
+
+    tract_url = f"https://www2.census.gov/geo/tiger/TIGER2019/TRACT/{tract_file}.zip"
+    download_file(tract_url, tract_url.split("/")[-1])
--- src/ziptool/load.py
+++ src/ziptool/load.py
@@ -1,28 +1,9 @@
-import tempfile
-import json
-import shutil
-import urllib
-from functools import lru_cache
-import os
-from os.path import exists
-from pathlib import Path
-from typing import Dict, List, Union
-import pkg_resources
-import tempfile
-
-import geopandas as gpd
-import numpy as np
 import pandas as pd
-import requests
-import us
-import wquantiles
+import pkg_resources
 
-pd.options.mode.chained_assignment = None
-
-from ziptool import shp_dir
 
 def load_crosswalk():
-    '''
+    """
     Loads in HUDS crosswalk file that is stored as a project resource.
 
     Args:
@@ -30,12 +11,15 @@ def load_crosswalk():
 
     Returns:
         A dataframe reprenting HUD crosswalk data.
-    '''
-    stream = pkg_resources.resource_stream(__name__, 'resources/ZIP_TRACT_122021.XLSX')
-    return pd.read_excel(stream)
+    """
+    stream = pkg_resources.resource_stream(
+        __name__, "resources/zip_tract_122021.parquet"
+    )
+    return pd.read_parquet(stream)
+
 
 def load_test_data():
-    '''
+    """
     Loads in test data that is used to verify the data processing pipeline's integrity.
 
     Args:
@@ -43,6 +27,6 @@ def load_test_data():
 
     Returns:
         A dataframe reprenting the sample data.
-    '''
-    stream = pkg_resources.resource_stream(__name__, 'resources/usa_00013.csv')
+    """
+    stream = pkg_resources.resource_stream(__name__, "resources/usa_00013.csv")
     return pd.read_csv(stream)
