d42196ec07a8d07875415a07272a7e41918b7996
joshuaneronha <77899869+joshuaneronha@users.noreply.github.com>
2022-03-10 10:36:52 -0500

    continuing docs...

--- docs/conf.py
+++ docs/conf.py
@@ -18,12 +18,12 @@
 source_suffix = ['.rst', '.md']
 
 
-import sphinx_rtd_theme
-
-extensions = [
-   "sphinx_rtd_theme",
-]
-html_theme = "sphinx_rtd_theme"
+# import sphinx_rtd_theme
+#
+# extensions = [
+#    "sphinx_rtd_theme",
+# ]
+# html_theme = "sphinx_rtd_theme"
 
 # -- Project information -----------------------------------------------------
 
