b401d4861cb4a1675772e43e48001110d236b126
joshuaneronha <77899869+joshuaneronha@users.noreply.github.com>
2022-03-11 12:08:29 -0500

    Update query_by_zip.py

--- src/ziptool/query_by_zip.py
+++ src/ziptool/query_by_zip.py
@@ -17,8 +17,7 @@ def data_by_zip(zips: List[str], acs_data, variables=None):
     Args:
         zips: a list of zipcodes, represented as strings i.e. ['02906', '72901', ...]
         acs_data: a string representing the path of the datafile OR a dataframe containing ACS datafile
-        variables:
-            To extract summary statistics, pass a dictionary of the form ::
+        variables: To extract summary statistics, pass a dictionary of the form: ::
 
                 {
                     variable_of_interest_1: {
